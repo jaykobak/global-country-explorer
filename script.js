7b5fbe89551543dbccd00fc2eb247eccb7374cb5
Jacob <akintolajacob75@gmail.com>
2026-04-28 20:21:09 +0100

    feat: get and store user's input as a variable

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // API Endpoint - https://restcountries.com/v3.1/name/{countryName}
 const apiEndpoint = "https://restcountries.com/v3.1/name/"
 
-async function searchCountry() {
-    console.log("working")
+const searchCountry = () =>  {
+    // Get user's input
+    const userInput = document.getElementById('countryInput').value
 }
