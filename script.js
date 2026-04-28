df59324ff13b1dffa494a78ad38c41163762ad01
Jacob <akintolajacob75@gmail.com>
2026-04-28 20:14:40 +0100

    feat: add onclick function to the search button

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // API Endpoint - https://restcountries.com/v3.1/name/{countryName}
 const apiEndpoint = "https://restcountries.com/v3.1/name/"
+
+async function searchCountry() {
+    console.log("working")
+}
