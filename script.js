fb34d6adedf4747e03891d5f8edf18e280ccadfd
Jacob <akintolajacob75@gmail.com>
2026-04-28 20:45:14 +0100

    feat: add input validation to the onclick of the search button

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ const apiEndpoint = "https://restcountries.com/v3.1/name/"
 const searchCountry = () =>  {
     // Get user's input
     const userInput = document.getElementById('countryInput').value
+
+    // Input validation
+    if (userInput.trim() === '') {
+        errorMsgElement = document.getElementById('errorMsg')
+        errorMsg.style.display = 'block'
+        errorMsg.innerText = 'Please enter a valid input!'
+    }
 }
