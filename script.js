ba0dc36ce3d58f136bcf319ba1eb9fcc41d3503e
Jacob <akintolajacob75@gmail.com>
2026-04-29 01:59:31 +0100

    feat: create async function for api data fetching

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // API Endpoint - https://restcountries.com/v3.1/name/{countryName}
 const apiEndpoint = "https://restcountries.com/v3.1/name/"
 
+async function getDataFromApi(countryName) {
+    try {
+        completeApiEndpoint = apiEndpoint + countryName
+        const countryInfo = await fetch(completeApiEndpoint)
+        const countryInfoJson = await countryInfo.json()
+        console.log(countryInfoJson)
+    }
+
+    catch(err) {
+        console.log(err)
+    }
+}
+
 const searchCountry = () =>  {
     // Get user's input
     const userInput = document.getElementById('countryInput').value
@@ -10,5 +23,8 @@ const searchCountry = () =>  {
         errorMsgElement = document.getElementById('errorMsg')
         errorMsg.style.display = 'block'
         errorMsg.innerText = 'Please enter a valid input!'
+        return;
     }
+
+    getDataFromApi(userInput)
 }
